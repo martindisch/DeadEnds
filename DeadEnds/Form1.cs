9db6a7e845c6f3e8036d5b01c660dacfb2d6b677
Martin Disch <martindisch@gmail.com>
2014-11-06 09:28:22 +0100

    Recursive searching of directories

--- DeadEnds/Form1.cs
+++ DeadEnds/Form1.cs
@@ -27,5 +27,42 @@ namespace DeadEnds
                 textBox1.Text = dg.SelectedPath;
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(textBox1.Text);
+                List<String> filenames = getFiles(directory);
+
+                foreach (String filename in filenames)
+                {
+                    ListViewItem item = new ListViewItem(new[] { filename, "0" });
+                    listView1.Items.Add(item);
+                }
+                MessageBox.Show("Replaced all extensions", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Select an extension", "Missing extension", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private List<String> getFiles(System.IO.DirectoryInfo directory)
+        {
+            List<String> filenames = new List<String>();
+            System.IO.DirectoryInfo[] directories = directory.GetDirectories();
+            System.IO.FileInfo[] files = directory.GetFiles();
+            foreach (System.IO.FileInfo file in files) {
+                filenames.Add(file.Name);
+            }
+            foreach (System.IO.DirectoryInfo dir in directories)
+            {
+                filenames.AddRange(getFiles(dir));
+            }
+            return filenames;
+        }
+
+
     }
 }
