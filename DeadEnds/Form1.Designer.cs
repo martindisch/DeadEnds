0fd105922840ef022044bb54383a55cabbeccad2
Martin Disch <martindisch@gmail.com>
2014-11-06 13:37:55 +0100

    Implemented usage search with starting point

--- DeadEnds/Form1.Designer.cs
+++ DeadEnds/Form1.Designer.cs
@@ -35,19 +35,23 @@
             this.names = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.timesUsed = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(310, 22);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(328, 11);
+            this.button1.Location = new System.Drawing.Point(328, 28);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -60,7 +64,7 @@
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.names,
             this.timesUsed});
-            this.listView1.Location = new System.Drawing.Point(12, 52);
+            this.listView1.Location = new System.Drawing.Point(12, 106);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(310, 184);
             this.listView1.TabIndex = 2;
@@ -80,7 +84,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(329, 205);
+            this.button2.Location = new System.Drawing.Point(329, 259);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 31);
             this.button2.TabIndex = 3;
@@ -88,11 +92,51 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(328, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Select";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(310, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Project folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Starting point";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 248);
+            this.ClientSize = new System.Drawing.Size(415, 302);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
@@ -116,6 +160,10 @@
         private System.Windows.Forms.ColumnHeader names;
         private System.Windows.Forms.ColumnHeader timesUsed;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- DeadEnds/Form1.cs
+++ DeadEnds/Form1.cs
@@ -12,6 +12,9 @@ namespace DeadEnds
 {
     public partial class Form1 : Form
     {
+
+        private List<System.IO.FileInfo> filenames;
+
         public Form1()
         {
             InitializeComponent();
@@ -36,18 +39,36 @@ namespace DeadEnds
             if (textBox1.Text != "")
             {
                 System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(textBox1.Text);
-                List<System.IO.FileInfo> filenames = getFiles(directory);
+                filenames = getFiles(directory);
+                List<System.IO.FileInfo> usedFiles = getUsage(new System.IO.FileInfo(textBox2.Text));
 
-                foreach (System.IO.FileInfo file in filenames)
+                /*foreach (System.IO.FileInfo file in filenames)
                 {
                     ListViewItem item = new ListViewItem(new[] { file.Name, "0" });
                     listView1.Items.Add(item);
-                }
+                }*/
                 MessageBox.Show("Checked usage of " + filenames.Count + " files", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                MessageBox.Show("Select a project folder", "Missing extension", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                MessageBox.Show("Select a project folder", "No folder", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                OpenFileDialog dg = new OpenFileDialog();
+                dg.InitialDirectory = textBox1.Text;
+                if (dg.ShowDialog() == DialogResult.OK)
+                {
+                    textBox2.Text = dg.FileName;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select a project folder first", "No folder", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
         }
 
@@ -56,7 +77,8 @@ namespace DeadEnds
             List<System.IO.FileInfo> filenames = new List<System.IO.FileInfo>();
             System.IO.DirectoryInfo[] directories = directory.GetDirectories();
             System.IO.FileInfo[] files = directory.GetFiles();
-            foreach (System.IO.FileInfo file in files) {
+            foreach (System.IO.FileInfo file in files)
+            {
                 filenames.Add(file);
             }
             foreach (System.IO.DirectoryInfo dir in directories)
@@ -66,6 +88,22 @@ namespace DeadEnds
             return filenames;
         }
 
+        private List<System.IO.FileInfo> getUsage(System.IO.FileInfo file)
+        {
+            List<System.IO.FileInfo> usedFiles = new List<System.IO.FileInfo>();
+            String content = System.IO.File.ReadAllText(file.FullName);
+            foreach (System.IO.FileInfo currentFile in filenames) {
+                if (content.Contains(currentFile.Name))
+                {
+                    if (!usedFiles.Contains(file))
+                    {
+                        usedFiles.Add(file);
+                        usedFiles.AddRange(getUsage(file));
+                    }
+                }
+            }
+            return usedFiles;
+        }
 
     }
 }
